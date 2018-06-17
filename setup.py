af5200dc0b45b6c4316d710893610b1cf9d5ced3
Brian Anichowski <32231334+banichow@users.noreply.github.com>
2018-06-17 17:11:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 plugin_identifier = "atxpihat"
 plugin_package = "octoprint_atxpihat"
 plugin_name = "OctoPrint-Atxpihat"
-plugin_version = "1.1.4"
+plugin_version = "1.1.5"
 plugin_description = """Octoprint Plugin for the ATXPiHat board"""
 plugin_author = "Brian Anichowski"
 plugin_author_email = "banichow@hotmail.com"
